c1607ea172359fd9d1a6bae48aee970c9f11cac1
Deutschball <212154162@qq.com>
2023-03-21 21:16:45 +0800

    base

---
+++ src/main/java/class4/spm/novelnook/controller/AdminController.java
@@ -0,0 +1,23 @@
+package class4.spm.novelnook.controller;
+
+import class4.spm.novelnook.pojo.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/user")
+    public User getUser(){
+        User user=new User("1","dustball","sjh","avatars/Takagi.jpg","Patron","example@qq.com","110");
+
+        return user;
+    }
+
+
+}
